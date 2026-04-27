2fbe4a34d5c27a8cc8105af22b2b3d80df9749e9
Jenish <jenishschaplot@gmail.com>
2026-04-28 01:09:04 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'memstore-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'logo.png'
+];
+
+// Install event: Cache the essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
